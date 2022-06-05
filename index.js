9d9749de18e4f697244652585bc030c68495e52b
Darshan Naicker <60061462+DuendeDigital@users.noreply.github.com>
2022-06-05 17:19:09 +0000

    Added Axios, Removed http

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const mySecret = process.env['KEY_TOKEN']
 console.log(mySecret);
 
 var x = 1;
+
+const axios = require('axios').default;
+
+axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
